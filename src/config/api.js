39dc4e420da62374ba9e87f5fb25ec5520fb0771
AhmedReact2023 <AhmedReactjs@gmail.com>
2023-08-12 20:57:53 +0300

    create Folder Config inside api

---
+++ src/config/api.js
@@ -0,0 +1,6 @@
+// export const Base_URL = "https://some-data.onrender.com";
+
+export const API_URL = process.env.REACT_APP_API_URL;
+export const GIPHY_URL = process.env.REACT_APP_GIPHY_URL;
+export const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+export const AUTH_API = process.env.REACT_APP_API;
