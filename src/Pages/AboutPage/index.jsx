3c8d208c63ed9c121bb49c31ad0cfa61ad9a823a
AhmedReact2023 <AhmedReactjs@gmail.com>
2023-08-12 20:59:09 +0300

    create Folder Aboutpages

---
+++ src/Pages/AboutPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+import { useThemeContext } from '../../Components/Context/ThemeContext';
+
+const AboutPage = () => {
+  return (
+    <div className='about-page'>
+      <h1>AboutPage</h1>
+    </div>
+  )
+}
+
+export default AboutPage;
